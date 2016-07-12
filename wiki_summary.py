f1b56b9c6572474a0d38284d046412c89cc0bc52
Mikerah <mikerah14@gmail.com>
2016-07-12 19:27:39 -0400

    refactored code

--- wiki_summary.py
+++ wiki_summary.py
@@ -1,12 +1,17 @@
 import argparse
+import wikipedia
 
-parser = argparse.ArgumentParser(description="Read Wikipedia Articles in your command prompt.")
+def create_optional_arguments(parser):
+    """ Given a parser, create optional arguments."""
 
-##### Creating paramaters #####
+    parser.add_argument('-R', '--random', help='display n random wikipedia articles (1 <= n <= 10)', \
+                        type=int, choices=[i for i in range(1,11)])
+                        
+    parser.add_argument('-r', '--read', help='display the specified summarized wikipedia article', type=str)
 
-parser.add_argument('-R', '--random', help='display n random wikipedia articles (1 <= n <= 10)', \
-                    type=int, choices=[i for i in range(1,11)])
-                    
-parser.add_argument('-r', '--read', help='display the specified summarized wikipedia article', type=str)
-
-args = parser.parse_args()
+    parser.add_argument('-rf', '--forever', help='display a random article until the user types stop')
+    
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="Read Wikipedia Articles in your command prompt.")
+    create_optional_arguments(parser)
+    args = parser.parse_args()
