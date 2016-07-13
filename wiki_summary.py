dc40010fba107e016dec5984f221ad94e24601f5
Mikerah <mikerah14@gmail.com>
2016-07-12 23:39:38 -0400

    completed the function where random articles are given until the user stops

--- wiki_summary.py
+++ wiki_summary.py
@@ -10,12 +10,12 @@ def create_optional_arguments(parser):
                         
     parser.add_argument('-r', '--read', help='display the specified summarized wikipedia article', type=str)
 
-    parser.add_argument('-rf', '--forever', help='display a random article until the user types stop')
+    parser.add_argument('-rf', '--forever', help='display a random article until the user types stop', action="store_true")
 
 def get_random_articles_v1(number_of_articles_wanted):
     """Given the wanted number of articles returned, get random wikipedia articles"""
     if number_of_articles_wanted == 1:
-        print(wikipedia.summary())
+        print(wikipedia.summary(wikipedia.random()))
     else:    
         list_of_articles = wikipedia.random(number_of_articles_wanted)
         try:
@@ -31,14 +31,26 @@ def get_random_articles_v1(number_of_articles_wanted):
             list_of_articles.remove(article)
             list_of_articles.append(wikipedia.random(article))
             
+def get_random_articles_v2():
+    """Retrieves random articles until the user types 'stop' """
+    ans = input('Press enter to continue or stop to stop: ')
+    while ans != 'stop':
+        try:
+            print(wikipedia.summary(wikipedia.random()))
+            print()
+            ans = input('Press enter to continue or stop to stop: ')
+        except wikipedia.exceptions.DisambiguationError:
+            print(wikipedia.summary(wikipedia.random()))
+            print()
+            ans = input('Press enter to continue or stop to stop: ')
 def choice(args):
     """ Given a namespace, determine what to do based of the parameters given. """
     if args.random:
-        return get_random_article_v1(args.random)
+        return get_random_articles_v1(args.random)
     elif args.read:
         return get_wanted_article(args.read)
     elif args.forever:
-        return get_random_articles_v2(args.forever)
+        return get_random_articles_v2()
         
     
 if __name__ == '__main__':
